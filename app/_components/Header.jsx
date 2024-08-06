07cbdcecf0fd00c66f81048a4a2ee459b78fd1df
Webstix Inc <git@webstix.com>
2024-08-06 14:56:37 +0530

    Clerk Auth completed

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -4,11 +4,18 @@ import Image from 'next/image'
 import { useEffect, useState } from "react"
 import { Button } from '@/components/ui/button'
 import Link from "next/link"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 function Header() {
   return (
-    <div className='p-3 shadow-sm flex justify-between'>
-    <div className='flex items-center gap-8 '>
+    <div className='p-3 shadow-sm flex justify-between align-middle'>
+    <div className='flex items-center gap-8'>
         <Link className='hover:cursor-pointer' href={'/'}><Image  src='/Anartby-logo.svg' alt='anartby' width={84} height={84} /></Link>
         <div className='md:flex items-center gap-6 hidden'>
             <Link href={'/'} className='hover:scale-105 hover:text-primary cursor-pointer'>Home</Link>
@@ -18,7 +25,18 @@ function Header() {
         </div>
     </div>
     <div>
-            <Button>Login / Sign Up</Button>
+      
+<div className='pt-6'>
+      <SignedOut>
+              <SignInButton type="button" className='text-white bg-primary hover:bg-orange-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-primary dark:hover:bg-orange-500 focus:outline-none dark:focus:ring-orange-400 '>Login / Sign Up</SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+
+            </div>
+            
+            
         </div>
     </div>
   )
