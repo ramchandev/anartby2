9b119f2d02ed561d8aeacfc5ad2f7f8010eee092
Webstix Inc <git@webstix.com>
2024-08-06 11:21:26 +0530

    Updated Images

--- app/(routes)/category/_components/CatDetails.jsx
+++ app/(routes)/category/_components/CatDetails.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import GlobalApi from '@/app/_services/GlobalApi';
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-
+import Image from "next/image";
 function CatDetails() {
     const params=usePathname();
     const catSlug=params.split('/')[2];
@@ -25,10 +25,24 @@ function CatDetails() {
 
     <div>{
         catdata.map((item,index)=>(
-            <div key={index} className=''>
-                <h2 className='font-bold text-[22px]'>{catdata[0].name}</h2>
+      
+            <div key={index} className='pb-10'>
+
+<div className='grid grid-cols-4 mt-8'>
+        <div className='col-span-1'>
+        <Image className='rounded' src={catdata[0].catImage.url} alt={catdata[0].name} width={216} height={216}/>
+
+        </div>
+    <div className='col-span-3'>
+    <h2 className='font-bold text-[22px]'>{catdata[0].name}</h2>
                 <div className='text-sm'>{catdata[0].description}</div>
                 <h2 className='font-bold pt-3'>Here are some of {catdata[0].name} curated paintings:</h2>
+       </div>
+    </div>
+
+
+               
+                
             </div>
         ))
         }</div>
